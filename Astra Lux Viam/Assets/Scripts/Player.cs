4f626ba99cf08fbdc70ad725fb20553d772aa5b9
theKasra <thekasra@outlook.com>
2020-03-14 08:01:54 +0330

    Player movement fixed
    By using ViewportToWorldPoint()

--- Astra Lux Viam/Assets/Scripts/Player.cs
+++ Astra Lux Viam/Assets/Scripts/Player.cs
@@ -7,12 +7,26 @@ public class Player : MonoBehaviour
 {
     // config params
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float xPadding = 1f;
+    [SerializeField] float yPadding = 1f;
+
+    float xMin, xMax;
+    float yMin, yMax;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x + xPadding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x - xPadding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0.05f, 0f)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0f, 1f, 0f)).y - yPadding;
     }
 
     // Update is called once per frame
@@ -25,8 +39,8 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
-        transform.position = new Vector2(newXPos, Mathf.Clamp(newYPos, -9f, -5f));
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
     }
 }

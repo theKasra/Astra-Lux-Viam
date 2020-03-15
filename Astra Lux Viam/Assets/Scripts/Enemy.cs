4e97f53d5a07ee8a78be477e2150ca13b09bf868
theKasra <thekasra@outlook.com>
2020-03-15 10:21:38 +0330

    Explosion particles added

--- Astra Lux Viam/Assets/Scripts/Enemy.cs
+++ Astra Lux Viam/Assets/Scripts/Enemy.cs
@@ -12,6 +12,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] float maxTimeBetweenShots = 3f;
     [SerializeField] GameObject projectile;
     [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfExplosion = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -61,7 +63,14 @@ public class Enemy : MonoBehaviour
         damageDealer.Hit();
         if (health <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        Destroy(explosion, durationOfExplosion);
     }
 }

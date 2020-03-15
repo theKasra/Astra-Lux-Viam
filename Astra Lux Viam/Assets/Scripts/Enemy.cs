37713d927209c6534e8a07a1c5f4b9ad0f2b6768
theKasra <thekasra@outlook.com>
2020-03-15 08:43:10 +0330

    Enemy shooting

--- Astra Lux Viam/Assets/Scripts/Enemy.cs
+++ Astra Lux Viam/Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,42 @@ public class Enemy : MonoBehaviour
 {
     // config params
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+            projectile,
+            transform.position,
+            Quaternion.identity) as GameObject;
+
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)

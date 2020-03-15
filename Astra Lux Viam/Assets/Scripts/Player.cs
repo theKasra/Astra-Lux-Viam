f383f9daf5d066b7f8ea925b0b7c74aace944058
theKasra <thekasra@outlook.com>
2020-03-15 05:52:07 +0330

    Player Shoot

--- Astra Lux Viam/Assets/Scripts/Player.cs
+++ Astra Lux Viam/Assets/Scripts/Player.cs
@@ -9,6 +9,11 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float xPadding = 1f;
     [SerializeField] float yPadding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
 
     float xMin, xMax;
     float yMin, yMax;
@@ -20,19 +25,39 @@ public class Player : MonoBehaviour
         SetUpMoveBoundaries();
     }
 
-    private void SetUpMoveBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x + xPadding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x - xPadding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0.05f, 0f)).y;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0f, 1f, 0f)).y - yPadding;
-    }
-
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+
+        if(Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            GameObject laser = Instantiate(
+                laserPrefab,
+                transform.position,
+                Quaternion.identity) as GameObject;
+
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+        
     }
 
     private void Move()
@@ -42,5 +67,14 @@ public class Player : MonoBehaviour
         var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x + xPadding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x - xPadding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0.05f, 0f)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0f, 1f, 0f)).y - yPadding;
     }
 }
